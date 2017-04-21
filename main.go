be34fb9a0cc0b55eac845b6d3c35a2c364e8e1d5
anjun <anjuncc@gmail.com>
2017-04-21 17:52:56 +0800

    tiker

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/anjunact/stock-monitor/crawler"
 
+	"time"
+	"fmt"
+	"log"
 )
 func main() {
 	 urls := []string{
@@ -12,7 +15,22 @@ func main() {
 		 "https://gupiao.baidu.com/stock/sh600717.html",
 	 }
 	for _, url := range urls{
-		 crawler.Scrape(url)
+		 //crawler.Scrape(url)
+		crawler.Test()
+		fmt.Println(url)
+	}
+	ticker()
+
+}
+func  ticker()  {
+	timer := time.NewTicker(2 * time.Second)
+	for {
+		select {
+		case <-timer.C:
+			go func() {
+				log.Println(time.Now())
+			}()
+		}
 	}
 }
 
